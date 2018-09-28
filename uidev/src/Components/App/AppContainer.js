39a13f65d26abd3ae9508ecfc172efe4cddace65
Seopftware <inseop0813@gmail.com>
2018-09-28 18:15:54 +0900

    Registering Local Node with Master Node

---
+++ uidev/src/Components/App/AppContainer.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { injectGlobal } from "styled-components";
+import reset from "styled-reset";
+import axios from "axios";
+import typography from "../../typography";
+import { MASTER_NODE, SELF_NODE, SELF_P2P_NODE } from "../../constants";
+import AppPresenter from "./AppPresenter";
+
+
+const baseStyles = () => injectGlobal`
+  ${reset};
+  ${typography};
+  h1,h2,h3,h4{
+    margin-bottom:0!important;
+  }
+`;
+ class AppContainer extends Component {
+  state = {
+    isLoading: true
+  };
+  componentDidMount = () => {
+    this._registerOnMaster();
+  };
+  render() {
+    baseStyles();
+    return <AppPresenter {...this.state} />;
+  }
+  _registerOnMaster = () => {
+    const request = axios.post(`${MASTER_NODE}/peers`, {
+      peer: SELF_P2P_NODE
+    });
+  };
+}
+ export default AppContainer;
--- uidev/src/index.js
+++ uidev/src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "Components/App";
+
 const { remote } = window.require("electron");
- const sharedPort = remote.getGlobal("sharedPort");
- alert(sharedPort);
- window.sharedPort = sharedPort;
- ReactDOM.render(<App />, document.getElementById("root"));
+const sharedPort = remote.getGlobal("sharedPort");
+
+window.sharedPort = sharedPort;
+ReactDOM.render(<App />, document.getElementById("root"));
